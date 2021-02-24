8174ad9c92699397f85edcaab55503be90415fac
Benginurozturk <77507752+Benginurozturk@users.noreply.github.com>
2021-02-25 02:23:12 +0300

    ***

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,11 +1,15 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using Core.Aspects.Autofac.Validation;
 using System.Text;
 
 namespace Business.Concrete
@@ -18,14 +22,13 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
+
             //business codes
-            if (product.ProductName.Length < 2) 
-            {
-                //magic strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //validation
+            
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
